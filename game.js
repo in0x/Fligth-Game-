601d5c70f6a4d07dcfb5672dd2a333ce8d46e2c6
Philipp Welsch <pwelsch.mmt-b2014@fh-salzburg.ac.at>
2015-05-03 23:28:09 +0200

    Added loading of Maya Exported OBJ

--- game.js
+++ game.js
@@ -17,6 +17,7 @@ window.onload = function() {
         } 
         document.addEventListener('keypress', function(e) {
         if (e.keyCode == 13) {
+          //element.webkitRequestFullscreen();
           element.requestPointerLock();
         }
           document.addEventListener( 'pointerlockchange', pointerlockchange, false );
@@ -37,28 +38,30 @@ window.onload = function() {
     scene.add( controls.getObject() );
 
     //First front left cube
-    var cube = new THREE.Mesh(new THREE.BoxGeometry(200, 200, 3000), new THREE.MeshLambertMaterial({
+    var cube = new THREE.Mesh(new THREE.BoxGeometry(100, 1000, 3000), new THREE.MeshLambertMaterial({
       color: 'blue' 
     }));
     cube.side = THREE.DoubleSide;
     cube.overdraw = true;
     cube.rotation.x = Math.PI * 0.1;
+    cube.translateX(-1200)
   
     //back lef cube
-    var cube1front = new THREE.Mesh(new THREE.BoxGeometry(200, 200, 3000), new THREE.MeshLambertMaterial({
+    var cube1front = new THREE.Mesh(new THREE.BoxGeometry(100, 1000, 3000), new THREE.MeshLambertMaterial({
       color: 'green' 
     }));
     cube1front.overdraw = true;
     cube1front.rotation.x = Math.PI * 0.1;
+    cube1front.translateX(-1200)
     cube1front.translateZ(-3000);
  
     // front right cube
     var cube2 = cube.clone();
-    cube2.translateX(600);
+    cube2.translateX(2400);
    
     // back right cube
     var cube2left = cube1front.clone();
-    cube2left.translateX(600);
+    cube2left.translateX(2400);
 
     // Light
     scene.add(new THREE.AmbientLight(0x333333));
@@ -69,29 +72,46 @@ window.onload = function() {
 
     //fog to mask reloading
     //scene.fog = new THREE.Fog('#0xeeeeee', 3000, 4000);
-    scene.fog = new THREE.FogExp2( 0x96e0e7, 0.0005 );
+    //scene.fog = new THREE.FogExp2( 0x96e0e7, 0.0005 );
+    scene.fog = new THREE.FogExp2( 0x96e0e7, 0.00025);
+
+    var loader = new THREE.JSONLoader();
+    function createScene(geometry, x, y, z, scale) {
+        zmesh = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial({color: 'green'}));
+        zmesh.position.set(x, y, z);
+        zmesh.scale.set(scale, scale, scale);
+        mesh_list.push(zmesh);
+        scene.add(zmesh);
+    }
+    var callback = function(geometry) {createScene(geometry, 0, 0, -80, 10 )};
+    loader.load("tree.js", callback);
 
 
     // My cube for detecting collisions with
-    var detectionBody = new THREE.Mesh(new THREE.BoxGeometry(50, 50, 50), new THREE.MeshLambertMaterial({color: 'blue'}));
+    var detectionBody = new THREE.Mesh(new THREE.BoxGeometry(20, 20, 20), new THREE.MeshLambertMaterial({color: 'blue'}));
     detectionBody.translateZ(-300);
     //detectionBody.rotation.y +=  45 * 180 / Math.PI;
-    scene.add(detectionBody);
-    camera.add(detectionBody);
+    //scene.add(detectionBody);
+    //camera.add(detectionBody);
+
+    var hitSphere = new THREE.Mesh(new THREE.SphereGeometry( 30, 32, 32), new THREE.MeshLambertMaterial({color: 'pink'}));
+    hitSphere.translateZ(-500);
+    hitSphere.translateX(300);
+    hitSphere.translateY(100);
+  
+    scene.add(cube);
+    scene.add(cube2);
+    scene.add(cube1front);
+    scene.add(cube2left); 
+    scene.add(hitSphere);
 
     //These are the objects to check raycasting intersection with
-    var obj_group = new THREE.Group();
-    obj_group.add(cube);
-    obj_group.add(cube2);
-    obj_group.add(cube1front);
-    obj_group.add(cube2left); 
-    scene.add(obj_group);
-
     var mesh_list = [];
     mesh_list.push(cube);
     mesh_list.push(cube2);
     mesh_list.push(cube1front);
     mesh_list.push(cube2left);
+    mesh_list.push(hitSphere);
    
     // Render
     var renderer = new THREE.WebGLRenderer({antialias: true});
@@ -107,7 +127,7 @@ window.onload = function() {
         mesh_list.forEach(function(mesh) {
           mesh.translateZ(-2000);
         });
-        lastCheckPosition = currentViewingPosition;
+      lastCheckPosition -= 1000;
       }
     }
     
@@ -122,8 +142,12 @@ window.onload = function() {
         //THREE.Raycaster( origin, direction, near, far)
         var raycaster = new THREE.Raycaster( controls.getObject().position, cameraDirection.clone().normalize(), 0, 100);
         var collisionResults = raycaster.intersectObjects(mesh_list, true);
-        if(collisionResults.length > 0)
+        if(collisionResults.length > 0 && collisionResults[0].distance < 30)
         {
+            if(collisionResults[0].object === hitSphere) {
+              console.log("Sphere hit!");
+              scene.remove(hitSphere);
+            }
             console.log('pew pew pew');
         }
         reloadWorld();
