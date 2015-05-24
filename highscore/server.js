4685c43a5cf0b86a1dd9e4d9ea5398aa9a7ed28d
Philipp Welsch <pwelsch.mmt-b2014@fh-salzburg.ac.at>
2015-05-24 17:43:37 +0200

    Made node api functional for OpenShift (now hosted)

---
+++ highscore/server.js
@@ -0,0 +1,90 @@
+var mongodb = require('mongodb');
+var monk = require('monk');
+var credentials = require('./credentials');
+var db = monk(credentials.uri);
+ 
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+var scores = require('./routes/scores');
+var top = require('./routes/top');
+var post = require('./routes/post');
+var http = require('http');
+var app = express();
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+    
+http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+app.use('/scores', scores);
+app.use('/top/:number', top);
+app.use('/post', post);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+app.setupVariables = function() {
+        //  Set the environment variables we need.
+        app.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+        app.port  = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
+        if (typeof app.ipaddress === "undefined") {
+            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
+            //  allows us to run/test the app locally.
+            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
+            app.ipaddress = "127.0.0.1";
+        };
+    };
+
+module.exports = app;
